91a99a8bb9c0c21fcfd6cf67c7cdee91ce59d3a9
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2025-01-31 18:26:20 +0100

    Update binary_to_SVG_Image.py
    #1 added multithread conversion
    
    >> python3 binary_to_SVG_Image.py <directory> <thread number>

--- Binary to Image/binary_to_SVG_Image.py
+++ Binary to Image/binary_to_SVG_Image.py
@@ -1,6 +1,10 @@
 import os
+import argparse
+import sys
 import numpy as np
 import svgwrite
+from queue import Queue
+from threading import Thread
 
 def read_binary_file(file_path):
     with open(file_path, 'rb') as f:
@@ -98,14 +102,35 @@ def convertBinary2RGBImage(input_file, output_file, scale=None):
     create_svg_rgb(rgb_array, output_file, width=rgb_width*10, height=rgb_height*10)
 
 
-def main():
-    file_path = '/home/dir/test.exe'
-    bytes_data = read_binary_file(file_path)
-    
-    base_name, _ = os.path.splitext(file_path)
-    directory = os.path.dirname(file_path)
-    convertBinary2RGBImage(file_path, os.path.join(directory, f"{base_name}_RGB.svg"))
-    convertBinary2GreyScaleImage(file_path, os.path.join(directory, f"{base_name}_Grayscale.svg"))
+def run(file_queue, width):
+    while not file_queue.empty():
+        file_path = file_queue.get()
+        base_name, _ = os.path.splitext(file_path)
+        directory = os.path.dirname(file_path)
+        convertBinary2RGBImage(file_path, os.path.join(directory, f"{base_name}_RGB.svg"))
+        convertBinary2GreyScaleImage(file_path, os.path.join(directory, f"{base_name}_Grayscale.svg"))
+        file_queue.task_done()
+
+
+def main(input_dir, thread_number=2, width=None):
+	# Get all executable files in input directory and add them into queue
+    file_queue = Queue()
+    for filename in os.listdir(input_dir):
+        file_path = os.path.join(input_dir, filename)
+        if os.path.isfile(file_path):
+            base_name, _ = os.path.splitext(filename)
+            file_queue.put(file_path)
+
+	# Start thread
+    for index in range(thread_number):
+        thread = Thread(target=run, args=(file_queue, width))
+        thread.daemon = True
+        thread.start()
+    file_queue.join()
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(prog='binary_to_SVG_Image.py', description="Convert binary file to SVG image")
+    parser.add_argument('input_dir', help='Input directory path is which include executable files')
+    parser.add_argument('thread_number', help='number of operation threads')
+    args = parser.parse_args()
+    main(args.input_dir, int(args.thread_number), width=None)
